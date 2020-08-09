f2faf165dd9b2e4e8985f14d47cb53a124658b68
DelissaB <delissacbeaman@gmail.com>
2020-08-08 21:36:56 -0500

    Hover red over buttons

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -6,9 +6,9 @@ interface ISidenavProps {}
 
 function Sidenav(props: ISidenavProps)  { 
   return (
-    <div className="buttons">
+    <div className="sidenav">
       <h1>Please select a floor!</h1>
-      <Link className="button" type="button" to="box6" smooth={true}duration={1000}> 6 </Link>
+     <Link className="button" type="button" to="box6" smooth={true}duration={1000}> 6 </Link>
       <Link className="button" type="button" to="box5" smooth={true}duration={2000}> 5 </Link>
       <Link className="button" type="button" to="box4" smooth={true}duration={3000}> 4 </Link>
       <Link className="button" type="button" to="box3" smooth={true}duration={4000}> 3 </Link>
