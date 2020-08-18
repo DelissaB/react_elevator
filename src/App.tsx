4254fb386c026d4384f15d854c7aa3fd510ee8c8
DelissaB <delissacbeaman@gmail.com>
2020-08-18 10:22:38 -0500

    Doors open over floor at page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import Sidenav from "./components/Sidenav";
-// import Floor from "./components/Floor";
-import Elevator from "./components/Elevator";
+import Floor from "./components/Floor";
+import LeftDoor from "./components/LeftDoor";
+import RightDoor from "./components/RightDoor";
+// import Elevator from "./components/Elevator";
 // import { Link, animateScroll as scroll }
 // from "react-scroll";
 
@@ -10,9 +12,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Floor /> */}
+        <LeftDoor />
+        <RightDoor />
+        <Floor />
         <Sidenav />
-        <Elevator />
+        {/* <Elevator /> */}
       </div>
     );
   }
