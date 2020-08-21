a9366837b2673327311b5569170819116d422866
DelissaB <delissacbeaman@gmail.com>
2020-08-21 00:04:05 -0500

    Sliding door on load

--- src/components/LeftDoor.js
+++ src/components/LeftDoor.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
-function LeftDoor() {
-  return <div className="leftDoor"></div>;
-}
+const LeftDoor = () => {
+  useEffect(() => {
+    AOS.init({ duration: 3000 });
+  }, []);
+  return (
+    <div className="App">
+      {/* <h1>Left Door Entering? </h1> */}
+      <div className="grids">
+        <div
+          data-aos="slide-right"
+          className="leftDoor"
+          data-aos-easing="ease-in-out"
+        >
+          1
+        </div>
+        {/* <div className="boxes">2</div> */}
+      </div>
+    </div>
+  );
+};
 
 export default LeftDoor;
--- src/components/RightDoor.js
+++ src/components/RightDoor.js
@@ -1,8 +1,27 @@
-import React from "react";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
-function RightDoor() {
-  return <div className="rightDoor"></div>;
-}
+const RightDoor = () => {
+  useEffect(() => {
+    AOS.init({ duration: 3000 });
+  }, []);
+  return (
+    <div className="App">
+      {/* <h1>Left Door Entering? </h1> */}
+      <div className="grids">
+        <div
+          data-aos="slide-left"
+          className="rightDoor"
+          data-aos-easing="ease-in-out"
+        >
+          1
+        </div>
+        {/* <div className="boxes">2</div> */}
+      </div>
+    </div>
+  );
+};
 
 export default RightDoor;
